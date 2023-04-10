e59daac1bb216830e5c0cb70a567b5eaaf9f2f84
iu-mattei <107809864+iu-mattei@users.noreply.github.com>
2023-04-11 02:59:57 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
 
+chat_id = 253630223 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: int, y: int) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    alpha = 0.01
+    stats, pval = stats.ttest_ind(x, y)
+    return pval <= alpha
