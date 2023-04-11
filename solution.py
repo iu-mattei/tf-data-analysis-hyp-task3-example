990fcb31b71246e6d81174ccb4154fc47c82a2a4
iu-mattei <107809864+iu-mattei@users.noreply.github.com>
2023-04-11 21:20:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,5 +6,5 @@ chat_id = 253630223 # Ваш chat ID, не меняйте название пе�
 
 def solution(x: int, y: int) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     alpha = 0.01
-    stat, pval = mannwhitneyu(x, y, alternative='two-sided')
-    return pval <= alpha
+    stat, pval = mannwhitneyu(x, y, alternative='less')
+    return pval < alpha
