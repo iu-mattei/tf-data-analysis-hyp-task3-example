f25c5523b9291b39e88fb7eff017bb6e1c0c9c69
iu-mattei <107809864+iu-mattei@users.noreply.github.com>
2023-04-11 20:26:41 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
-import scipy.stats as stats
+from scipy.stats import mannwhitneyu
 
 chat_id = 253630223 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: int, y: int) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     alpha = 0.01
-    st, pval = stats.ttest_ind(x, y)
+    stat, pval = mannwhitneyu(x, y, alternative='two-sided')
     return pval <= alpha
